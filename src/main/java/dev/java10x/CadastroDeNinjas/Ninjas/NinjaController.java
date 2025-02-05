4ce4f125e9ace6c2965218cdc9383c54dfeb414f
HugoAuroraTech <hugovinidc@gmail.com>
2025-02-05 19:58:16 -0300

    feat: Adiciona a logica de listar todos os ninjas

--- src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -4,12 +4,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
+@RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boas-vindas")
     public String boasVindas(){
         return "Bem vindo";
+    }
+
+    @GetMapping("/listar")
+    public List<NinjaModel> listarNinjas() {
+        return this.ninjaService.listarNinjas();
     }
 }
