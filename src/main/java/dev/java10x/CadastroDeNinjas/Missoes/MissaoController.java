87189d175b98822c26c98dc7da33354f8e18ded2
HugoAuroraTech <hugovinidc@gmail.com>
2025-02-05 19:59:24 -0300

    fix: Resolve o loop do json

--- src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
+++ src/main/java/dev/java10x/CadastroDeNinjas/Missoes/MissaoController.java
@@ -1,4 +1,12 @@
 package dev.java10x.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/missoes")
 public class MissaoController {
+
+
 }
